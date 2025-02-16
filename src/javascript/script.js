7abe92e04491689b0ef3e47ef240f6192d28057f
sophxs <115382818+sophxs@users.noreply.github.com>
2025-02-16 03:37:37 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,21 @@
+let targetDate = new Date("Apr 05, 2025 00:00:00").getTime();
+
+daysEle = document.getElementById("dias");
+hoursEle = document.getElementById("horas");
+minutesEle = document.getElementById("minutos");
+secondsEle = document.getElementById("segundos");
+
+let countdownTimer = setInterval (() => {
+  let now = new Date().getTime();
+  let timeDiff = targetDate - now;
+
+  let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(((timeDiff - 1000 * 60 * 60 * 24 * days) % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor ((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor ((timeDiff % (1000 * 60)) / 1000);
+
+  daysEle.innerText = days;
+  hoursEle.innerText = hours;
+  minutesEle.innerText = minutes;
+  secondsEle.innerText = seconds;
+});
